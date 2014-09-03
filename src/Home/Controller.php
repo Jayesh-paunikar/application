a5e9433ce2f34cfa2558121447b553011e099481
devosc <devosc@gmail.com>
2014-09-03 11:27:25 -0500

    ci

--- src/Home/Controller.php
+++ src/Home/Controller.php
@@ -2,11 +2,7 @@
 
 namespace Application\Home;
 
-use Framework\Controller\Controller\EventInterface;
 use Framework\View\Model\ServiceTrait as ViewModel;
-use Framework\View\Model\ModelInterface as ViewModelInterface;
-use Framework\Request\RequestInterface;
-use Framework\Response\ResponseInterface;
 
 class Controller
     implements ControllerInterface
@@ -17,12 +13,9 @@ class Controller
     use ViewModel;
 
     /**
-     * @param EventInterface $event
-     * @param RequestInterface $request
-     * @param ResponseInterface $response
-     * @return ViewModelInterface
+     * @return mixed
      */
-    public function __invoke(EventInterface $event, RequestInterface $request, ResponseInterface $response)
+    public function __invoke()
     {
         return $this->viewModel();
     }
--- src/Home/ControllerInterface.php
+++ src/Home/ControllerInterface.php
@@ -2,17 +2,6 @@
 
 namespace Application\Home;
 
-use Framework\Controller\Controller\EventInterface;
-use Framework\Request\RequestInterface;
-use Framework\Response\ResponseInterface;
-
 interface ControllerInterface
 {
-    /**
-     * @param EventInterface $event
-     * @param RequestInterface $request
-     * @param ResponseInterface $response
-     * @return mixed
-     */
-    public function __invoke(EventInterface $event, RequestInterface $request, ResponseInterface $response);
 }
