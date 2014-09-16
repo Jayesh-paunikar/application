0ef3fc3412806bd585ed3a647c96febb42a9c05d
devosc <devosc@gmail.com>
2014-09-16 04:47:44 -0500

    config array access

--- config/view.php
+++ config/view.php
@@ -3,15 +3,13 @@
  *
  */
 
-use Framework\Config\Config as Config;
-
 return [
-    'templates' => new Config([
+    'templates' => [
         'layout'          => __DIR__ . '/../view/layout/layout.phtml',
         'error/exception' => __DIR__ . '/../vendor/mvc5/framework/src/View/Exception/exception.phtml',
         'error/404'       => __DIR__ . '/../view/error/404.phtml',
         'home'            => __DIR__ . '/../view/home/index.phtml',
-    ]),
+    ],
     'aliases' => [
         'url'       => 'Route\Generator\Plugin',
         'translate' => 'Translator\Plugin',
