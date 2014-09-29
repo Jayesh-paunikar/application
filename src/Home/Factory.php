a5eccd4f6c84d069c7f741222d5b430f8d9529ee
devosc <devosc@gmail.com>
2014-09-29 17:26:32 -0500

    demo Home\Factory class

---
+++ src/Home/Factory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Home;
+
+use Framework\Service\Factory\Factory as Base;
+
+class Factory
+    extends Base
+{
+    /**
+     * @return Controller
+     */
+    public function __invoke()
+    {
+        /**
+         * @var \Framework\View\Manager\ManagerInterface $viewManager
+         * @var \Framework\Config\ConfigInterface $config
+         */
+
+        $config      = $this->config();
+        $viewManager = $this->get('View\Manager');
+
+        $vm = new ViewModel;
+        $vm->setTemplate($config->get('view')->get('templates')['home']); //nicer if had $this->param()
+        $vm->setViewManager($viewManager);
+
+        $controller = new Controller;
+        $controller->setViewModel($vm);
+
+        return $controller;
+    }
+}
