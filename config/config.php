21b18e9b3dfee0bc535be9228e885137e1dc9560
devosc <devosc@gmail.com>
2014-11-27 19:36:53 -0600

    ci

--- config/config.php
+++ config/config.php
@@ -10,10 +10,8 @@ use Framework\Event\Config\Events;
 
 return [
     'alias'     => include __DIR__ . '/alias.php',
-    //'events'    => new Events(include __DIR__ . '/event.php'),
-    'events'    => new Events(include __DIR__ . '/../vendor/mvc5/framework/test/config/event.php'),
+    'events'    => new Events(include __DIR__ . '/event.php'),
     'services'  => new Container(include __DIR__ . '/service.php'),
-    //'routes'    => new RouteDefinition(include __DIR__ . '/route.php'),
-    'routes'    => new RouteDefinition(include __DIR__ . '/../vendor/mvc5/framework/test/config/route.php'),
+    'routes'    => new RouteDefinition(include __DIR__ . '/route.php'),
     'templates' => new Config(include __DIR__ . '/templates.php')
 ];
