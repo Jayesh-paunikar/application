76154d628aea395ddfbc92a7a05c5d5840c1a077
devosc <devosc@gmail.com>
2014-12-29 02:03:44 -0600

    ci

--- init.php
+++ init.php
@@ -199,9 +199,9 @@ include __DIR__ . '/src/Response/HttpResponse.php';
 /**
  *
  */
-include __DIR__ . '/vendor/mvc5/framework/src/View/Model/Model.php';
-include __DIR__ . '/src/Home/Home.php';
-include __DIR__ . '/src/Home/Controller.php';
+//include __DIR__ . '/vendor/mvc5/framework/src/View/Model/Model.php';
+//include __DIR__ . '/src/Home/Home.php';
+//include __DIR__ . '/src/Home/Controller.php';
 
 /**
  *
