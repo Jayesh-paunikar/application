5415048b4c2e523feccf09626e22cecff622fd5c
devosc <devosc@gmail.com>
2015-01-26 01:15:18 -0600

    ci

--- src/Home/Controller.php
+++ src/Home/Controller.php
@@ -2,10 +2,8 @@
 
 namespace Home;
 
-use Framework\View\Manager\ViewManager;
 use Framework\View\ViewModel as View;
 use Framework\View\Model\ViewModel;
-//use Framework\View\Model\Model;
 use Request\Request;
 use Response\Response;
 
@@ -24,37 +22,6 @@ class Controller
     {
         $this->model = $model;
         $model && !$model->path() && $model->template('home');
-    }
-
-    /**
-     * @return mixed
-     */
-    public static function staticCall()
-    {
-        var_dump(__FUNCTION__.':'.__FILE__, func_get_args());
-    }
-
-    /**
-     * @param ViewManager $vm
-     * @param array $args
-     * @return mixed
-     */
-    public static function staticTest(ViewManager $vm, array $args = [])
-    {
-        $args['args'][] = __FUNCTION__;
-
-        return new Model('home', $args);
-    }
-
-    /**
-     * @param Response $response
-     * @param Request $request
-     * @param array $args
-     * @return ViewModel
-     */
-    public function test(Response $response, Request $request, array $args = [])
-    {
-        return $this->view('../view/home/index.phtml', $args + ['args' => [__FUNCTION__]]);
     }
 
     /**
