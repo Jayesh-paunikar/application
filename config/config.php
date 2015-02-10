1ca5d177317df030c11a48a26520903481174c24
devosc <devosc@gmail.com>
2015-02-10 00:52:34 -0600

    ci

--- config/config.php
+++ config/config.php
@@ -3,7 +3,6 @@
  *
  */
 
-use Framework\Config\Config;
 use Framework\Route\Definition\RouteDefinition;
 use Framework\Service\Container\Container;
 
@@ -12,5 +11,5 @@ return [
     'events'    => include __DIR__ . '/event.php',
     'services'  => new Container(include __DIR__ . '/service.php'),
     'routes'    => new RouteDefinition(include __DIR__ . '/route.php'),
-    'templates' => new Config(include __DIR__ . '/templates.php')
+    'templates' => include __DIR__ . '/templates.php'
 ];
