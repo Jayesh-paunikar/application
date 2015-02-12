7025e83fe2c2c59a3be9e3e1a4d545b7fc78ffa3
devosc <devosc@gmail.com>
2015-02-11 22:36:51 -0600

    allow array route config

--- config/config.php
+++ config/config.php
@@ -3,13 +3,12 @@
  *
  */
 
-use Framework\Route\Definition\RouteDefinition;
 use Framework\Service\Container\Container;
 
 return [
     'alias'     => include __DIR__ . '/alias.php',
     'events'    => include __DIR__ . '/event.php',
     'services'  => new Container(include __DIR__ . '/service.php'),
-    'routes'    => new RouteDefinition(include __DIR__ . '/route.php'),
+    'routes'    => include __DIR__ . '/route.php',
     'templates' => include __DIR__ . '/templates.php'
 ];
