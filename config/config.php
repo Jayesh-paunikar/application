cd5ac9071d67690f5aaf179b610b6799f9778535
devosc <devosc@gmail.com>
2015-10-27 20:42:29 -0500

    Container is needed for shared objects.

--- config/config.php
+++ config/config.php
@@ -3,10 +3,12 @@
  *
  */
 
+use Mvc5\Service\Container\Container;
+
 return [
     'alias'     => include __DIR__ . '/alias.php',
     'events'    => include __DIR__ . '/event.php',
-    'services'  => include __DIR__ . '/service.php',
+    'services'  => new Container(include __DIR__ . '/service.php'),
     'routes'    => include __DIR__ . '/route.php',
     'templates' => include __DIR__ . '/templates.php'
 ];
